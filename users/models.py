e56786b2ff14ce9e19f7fd141e612409bd007888
Vika <victoria.gaponava@gmail.com>
2024-01-01 21:33:57 +0300

    added field chat_id

--- users/models.py
+++ users/models.py
@@ -9,6 +9,9 @@ class EmployeeUser(AbstractUser):
         unique=True, verbose_name="электронная почта пользователя"
     )
     position = models.CharField(max_length=50, verbose_name='должность')
+    chat_id = models.CharField(
+        max_length=20, unique=True, default=0, verbose_name="id пользователя в телеграм"
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
