b2c1a805ccbbfaca397326706e4c89c3b19fbbce
Vika <victoria.gaponava@gmail.com>
2024-01-01 21:27:41 +0300

    Create model for employee

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class EmployeeUser(AbstractUser):
+    employee_id = models.CharField(max_length=10, unique=True, verbose_name='идентификатор сотрудника')
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="электронная почта пользователя"
+    )
+    position = models.CharField(max_length=50, verbose_name='должность')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
