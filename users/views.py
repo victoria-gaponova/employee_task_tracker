69aa520b893583166cd331036fdc32ab54eb66d9
Vika <victoria.gaponava@gmail.com>
2024-01-02 01:26:51 +0300

    Fixed views for EmployeeUser

--- users/views.py
+++ users/views.py
@@ -7,18 +7,29 @@ from users.serializers import EmployeeUserSerializer
 class EmployeeUserListCreateView(generics.ListCreateAPIView):
     """Представление для создания  и просмотра списка задач сотрудника.
     Attributes:
-        queryset (QuerySet): Набор объектов задач.
-        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов задач в JSON.
+        queryset (QuerySet): Набор объектов сотрудников.
+        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
     """
     queryset = EmployeeUser.objects.all()
     serializer_class = EmployeeUserSerializer
 
 
 class EmployeeUserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Представление для просмотра, обновления и удаления задачи
+    """Представление для просмотра, обновления и удаления задачи.
     Attributes:
-        queryset (QuerySet): Набор объектов задач.
-        serializer_class (LessonSerializer): Сериализатор, используемый для преобразования объектов задач в JSON.
+        queryset (QuerySet): Набор объектов сотрудников.
+        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
     """
     queryset = EmployeeUser.objects.all()
+    serializer_class = EmployeeUserSerializer
+
+
+class BusyEmployeesUserListView(generics.ListAPIView):
+    """
+    Представление для просмотра списка занятых сотрудников.
+    Attributes:
+        queryset (QuerySet): Набор объектов сотрудников.
+        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
+    """
+    queryset = EmployeeUser.objects.filter(is_busy=True)
     serializer_class = EmployeeUserSerializer
