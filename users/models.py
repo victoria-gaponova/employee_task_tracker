796b0f9c52e4e11c4e5e5a0bfb9d37d8f1afb6d3
Vika <victoria.gaponava@gmail.com>
2024-01-02 01:26:16 +0300

    Fixed model for EmployeeUser

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class EmployeeUser(AbstractUser):
     """
-        Модель сотрудника с доаолнительными полями.
+        Модель сотрудника с дополнительными полями.
 
         Дополнительные поля: employee_id, full_name, position, chat_id.
 
@@ -13,7 +13,7 @@ class EmployeeUser(AbstractUser):
         Attributes:
             employee_id (str): Идентификатор сотрудника.
             full_name (str): ФИО сотрудника.
-            email (str): Уникальный адрес электронной почты пользователя.
+            email (str): Уникальный адрес электронной почты сотрудника.
             position (str): Должность сотрудника.
             chat_id(str): id чата в tg
         """
@@ -21,12 +21,13 @@ class EmployeeUser(AbstractUser):
     username = None
     full_name = models.CharField(max_length=255, default= 'undefined',verbose_name='ФИО')
     email = models.EmailField(
-        unique=True, verbose_name='электронная почта пользователя'
+        unique=True, verbose_name='электронная почта сотрудника'
     )
     position = models.CharField(max_length=50, verbose_name='должность')
     chat_id = models.CharField(
         max_length=20, unique=True, default=0, verbose_name="id пользователя в телеграм"
     )
+    is_busy = models.BooleanField(default=False, help_text="признак занятости сотрудника")
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
