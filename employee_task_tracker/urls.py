56ff833ea532dba5e7c1abefbc409d3266cdcd54
Vika <victoria.gaponava@gmail.com>
2024-01-02 01:27:49 +0300

    Added url for user

--- employee_task_tracker/urls.py
+++ employee_task_tracker/urls.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -20,10 +21,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users.urls")),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
 ]
