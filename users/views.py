ac0b3d906eb762be932dbf394672b7296cb558af
Vika <victoria.gaponava@gmail.com>
2024-01-02 00:30:56 +0300

    Created views for model EmployeeUser

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import EmployeeUser
+from users.serializers import EmployeeUserSerializer
+
+
+class EmployeeUserListCreateView(generics.ListCreateAPIView):
+    """Представление для создания  и просмотра списка задач сотрудника.
+    Attributes:
+        queryset (QuerySet): Набор объектов задач.
+        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов задач в JSON.
+    """
+    queryset = EmployeeUser.objects.all()
+    serializer_class = EmployeeUserSerializer
+
+
+class EmployeeUserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Представление для просмотра, обновления и удаления задачи
+    Attributes:
+        queryset (QuerySet): Набор объектов задач.
+        serializer_class (LessonSerializer): Сериализатор, используемый для преобразования объектов задач в JSON.
+    """
+    queryset = EmployeeUser.objects.all()
+    serializer_class = EmployeeUserSerializer
