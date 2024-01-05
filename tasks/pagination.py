8a2c48c51d4e953965a2aee52b1a58208b6cbdcb
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:33:03 +0300

    Pagination for task

---
+++ tasks/pagination.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class TaskPagination(PageNumberPagination):
+    page_size = 5  # Количество элементов на странице
+    page_size_query_param = (
+        "page_size"  # Параметр запроса для указания количества элементов на странице
+    )
+    max_page_size = 10  # Максимальное количество элементов на странице
