3df9254ec9d7a0c55e25263315076258bcb668c4
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:27:18 +0300

    Changed urls for user

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import EmployeeViewSet
 
-from users.apps import UsersConfig
-from users.views import EmployeeUserListCreateView, EmployeeUserDetailView, BusyEmployeesUserListView
-
-app_name = UsersConfig.name
+# Создаем экземпляр DefaultRouter
+router = DefaultRouter()
+# Регистрируем EmployeeViewSet с именем 'employees'
+router.register(r"employees", EmployeeViewSet, basename="employee")
 
 urlpatterns = [
-    path('employees/', EmployeeUserListCreateView.as_view(), name='employee-list-create'),
-    path('<int:pk>/employee/', EmployeeUserDetailView.as_view(), name='employee-detail'),
-    path('employees/busy/', BusyEmployeesUserListView.as_view(), name='employee_busy-list')
+    path("", include(router.urls)),
 ]
