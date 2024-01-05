9bccc216135de43ea83b047d0ee658a4e3507fc6
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:25:34 +0300

    Changed serializer for user

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,26 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from tasks.serializers import TaskSerializer
+from users.models import Employee
 
-class EmployeeUserSerializer(serializers.ModelSerializer):
+
+class EmployeeSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели сотрудника.
 
     Attributes:
-        employee_id (str): Уникальный идентификатор сотрудника.
-        full_name (str): ФИО сотрудника.
+        id (int): Уникальный идентификатор сотрудника.
+        full_name (str): Полное имя сотрудника.
         email (str): Адрес электронной почты сотрудника.
         position (str): Должность сотрудника.
-        chat_id (str): id чата сотрудника в telegram.
+        chat_id (str): Идентификатор сотрудника в Telegram.
 
+    Поле tasks:
+        Список задач, связанных с текущим сотрудником.
     """
+
+    tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
-        model = get_user_model()
-        fields = ['employee_id', 'full_name', 'email', 'position', 'chat_id']
+        model = Employee
+        fields = ["id", "full_name", "email", "position", "chat_id", "tasks"]
