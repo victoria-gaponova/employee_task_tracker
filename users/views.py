6d820d22b24d5dd702efe5ff57f2ef1a2a404a17
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:26:05 +0300

    Changed views for user

--- users/views.py
+++ users/views.py
@@ -1,35 +1,49 @@
-from rest_framework import generics
+from django.db.models import Count, Q
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from users.models import EmployeeUser
-from users.serializers import EmployeeUserSerializer
+from users.models import Employee
+from users.pagination import EmployeePagination
+from users.serializers import EmployeeSerializer
 
 
-class EmployeeUserListCreateView(generics.ListCreateAPIView):
-    """Представление для создания  и просмотра списка задач сотрудника.
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    Представление для взаимодействия с моделью сотрудника.
+
     Attributes:
-        queryset (QuerySet): Набор объектов сотрудников.
-        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
-    """
-    queryset = EmployeeUser.objects.all()
-    serializer_class = EmployeeUserSerializer
+        queryset (QuerySet): Набор данных сотрудников, используемых в представлении.
+        serializer_class (EmployeeSerializer): Сериализатор для сотрудников.
+        pagination_class (EmployeePagination): Класс пагинации для данного представления.
 
+    Methods:
+        busy_employees: Пользовательское действие для получения списка занятых сотрудников.
 
-class EmployeeUserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """Представление для просмотра, обновления и удаления задачи.
-    Attributes:
-        queryset (QuerySet): Набор объектов сотрудников.
-        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
     """
-    queryset = EmployeeUser.objects.all()
-    serializer_class = EmployeeUserSerializer
 
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    pagination_class = EmployeePagination
 
-class BusyEmployeesUserListView(generics.ListAPIView):
-    """
-    Представление для просмотра списка занятых сотрудников.
-    Attributes:
-        queryset (QuerySet): Набор объектов сотрудников.
-        serializer_class (EmployeeUserSerializer): Сериализатор, используемый для преобразования объектов сотрудников в JSON.
-    """
-    queryset = EmployeeUser.objects.filter(is_busy=True)
-    serializer_class = EmployeeUserSerializer
+    @action(detail=False, methods=["get"])
+    def busy_employees(self, request):
+        """
+        Получение списка занятых сотрудников.
+
+        Parameters:
+            request (Request): Запрос от клиента.
+
+        Returns:
+            Response: JSON-ответ с информацией о занятых сотрудников.
+
+        """
+        employees = (
+            Employee.objects.annotate(
+                active_tasks_count=Count("tasks", filter=Q(tasks__status="в работе"))
+            )
+            .filter(active_tasks_count__gt=0)
+            .order_by("-active_tasks_count")
+        )
+        serializer = self.get_serializer(employees, many=True)
+        return Response(serializer.data)
