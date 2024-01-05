17a4b5591ace5b1998d9c180586ba42fef9e0841
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:39:21 +0300

    Changed urls employee_task_tracker

--- employee_task_tracker/urls.py
+++ employee_task_tracker/urls.py
@@ -1,10 +1,8 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,11 +20,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("", include("tasks.urls")),
     path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-
 ]
