38899311b23e3e32d9182f8c38a0015fc834095a
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:42:47 +0300

    Created admin for task

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "status", "employee")
+    search_fields = ("name", "employee__full_name")
+    list_filter = ("status", "deadline")
+    ordering = ("deadline", "name")
+    raw_id_fields = ("employee", "parent_task")
