b676ef84bfb2c1b4be7586f97e9e8e694ee2af67
Vika <victoria.gaponava@gmail.com>
2024-01-06 00:42:40 +0300

    Created admin for user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "position")
+    search_fields = ("full_name", "email", "position")
+    list_filter = ("position",)
+    ordering = ("full_name",)
+    exclude = (
+        "password",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "groups",
+        "user_permissions",
+        "last_login",
+        "date_joined",
+    )
